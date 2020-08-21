b9d5af7d6cbebba89015501ed5a3431fa6ae81f2
Gururaj <gururaj@getstarted.in>
2020-08-21 16:55:26 +0530

    connected app.js and modifying html via JS

---
+++ static/app.js
@@ -0,0 +1,6 @@
+var output1=document.querySelector('#output1');
+var btn1=document.querySelector('#btn1');
+
+btn1.addEventListener("click",function(){
+    output1.innerText="You Clicked Below button";
+});
